47a2acfaf6dc9276eabab43279abdc6d728d2d75
Nazaryan ART <84046495+SL1dee36@users.noreply.github.com>
2024-05-19 18:02:08 +0300

    Update detect_fire.py

--- functions/detect_fire.py
+++ functions/detect_fire.py
@@ -13,12 +13,8 @@ def detect_fire(frame, dbg, display_mode, hexagon_size):
             h_s = cv2.getTrackbarPos("HIGH_1", "Trackbars")
             h_v = cv2.getTrackbarPos("HIGH_2", "Trackbars")
     except:
-        l_h = 0
-        l_s = 100
-        l_v = 255
-        h_h = 25
-        h_s = 255
-        h_v = 255
+        l_h,l_s,l_v = 0,100,255
+        h_h,h_s,h_v = 25,255,255
 
     lower_fire = np.array([l_h, l_s, l_v])
     upper_fire = np.array([h_h, h_s, h_v])
@@ -30,23 +26,17 @@ def detect_fire(frame, dbg, display_mode, hexagon_size):
     for contour in contours:
         if cv2.contourArea(contour) > hexagon_size * hexagon_size:
             x, y, w, h = cv2.boundingRect(contour)
-            # Если мы дошли до этого места, значит, маленький квадрат не находится внутри большого
             if display_mode == 1:
-                # Проверяем, находится ли центр маленького квадрата внутри большого
                 center = (x + w//2, y + h//2)
                 for big_contour in contours:
                     if cv2.pointPolygonTest(big_contour, center, False) > 0:
-                        # Если центр маленького квадрата находится внутри большого, пропускаем его
                         continue
-
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
             elif display_mode == 2:
                 M = cv2.moments(contour)
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
                 draw_hexagon(frame, (cX, cY), hexagon_size, (0, 255, 0))
-
             elif display_mode == 0:
                 cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
     return frame, contours
